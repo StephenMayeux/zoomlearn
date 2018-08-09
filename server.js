a46a8d324b6120f7942f7efb3872d698569372e3
Stephen Mayeux <stephenmayeux@gmail.com>
2018-08-09 00:11:33 -0500

    another fix

--- server.js
+++ server.js
@@ -10,10 +10,12 @@ io.sockets.on('connection', routes);
 
 app.set('port', process.env.PORT || 3001);
 
-  app.use(express.static(path.join(__dirname, 'client', 'build')));
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+}
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+app.get('*', (request, response) => {
+	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
 server.listen(app.get('port'), () => {
