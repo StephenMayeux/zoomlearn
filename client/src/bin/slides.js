a957059dea79bc10e2dce2b1d9ffd25c44d60a75
Stephen Mayeux <stephenmayeux@gmail.com>
2018-08-11 10:22:10 -0500

    new build

--- client/src/bin/slides.js
+++ client/src/bin/slides.js
@@ -53,6 +53,96 @@ const slides = [
     id: 10,
     hanzi: '更',
     pinyin: 'gèng'
+  },
+  {
+    id: 11,
+    hanzi: '太贵了',
+    pinyin: 'Tài guile'
+  },
+  {
+    id: 12,
+    hanzi: '韩国',
+    pinyin: 'Hánguó'
+  },
+  {
+    id: 13,
+    hanzi: '加拿大',
+    pinyin: 'Jiānádà'
+  },
+  {
+    id: 14,
+    hanzi: '澳大利亚',
+    pinyin: 'Àodàlìyǎ'
+  },
+  {
+    id: 15,
+    hanzi: '法国',
+    pinyin: 'Fàguó'
+  },
+  {
+    id: 16,
+    hanzi: '日本',
+    pinyin: 'Rìběn'
+  },
+  {
+    id: 17,
+    hanzi: '大辣',
+    pinyin: 'Dà là'
+  },
+  {
+    id: 18,
+    hanzi: '中辣',
+    pinyin: 'zhōng là'
+  },
+  {
+    id: 19,
+    hanzi: '小辣',
+    pinyin: 'Xiǎo là'
+  },
+  {
+    id: 20,
+    hanzi: '都',
+    pinyin: 'Dōu'
+  },
+  {
+    id: 21,
+    hanzi: '健康',
+    pinyin: 'Jiànkāng'
+  },
+  {
+    id: 22,
+    hanzi: '知道',
+    pinyin: 'Zhì dào'
+  },
+  {
+    id: 23,
+    hanzi: '永远',
+    pinyin: 'Yǒngyuǎn'
+  },
+  {
+    id: 24,
+    hanzi: '点',
+    pinyin: 'Diǎn'
+  },
+  {
+    id: 25,
+    hanzi: '咸',
+    pinyin: 'Xián'
+  },
+  {
+    id: 26,
+    hanzi: '几',
+    pinyin: 'Jǐ'
+  },
+  {
+    id: 27,
+    hanzi: '海',
+    pinyin: 'Hǎi'
+  },
+  {
+    id: 28,
+    hanzi: '都少',
+    pinyin: 'Dōu shǎo'
   }
 ];
 
